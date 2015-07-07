f5cc57556d5d94c6690deb66f86df49e4d674a43
ESC <youhavethewrong@gmail.com>
2015-07-06 22:36:03 -0500

    Added an analytical optimization for a small forward circuit.

--- src/rnn/core.clj
+++ src/rnn/core.clj
@@ -1,8 +1,17 @@
 (ns rnn.core)
 
-(defn forward-multiplier-gate
+(defn forward-multiply-gate
   [x y]
   (* x y))
+
+(defn forward-add-gate
+  [x y]
+  (+ x y))
+
+(defn forward-circuit
+  [x y z]
+  (forward-multiply-gate
+   (forward-add-gate x y) z))
 
 (defn random-tweak
   [f x y]
@@ -15,7 +24,7 @@
         {:result best-result :x best-x :y best-y}
         (let [try-x (+ x (* tweak-amount (dec (* (rand) 2))))
               try-y (+ y (* tweak-amount (dec (* (rand) 2))))
-              try-result (forward-multiplier-gate try-x try-y)
+              try-result (forward-multiply-gate try-x try-y)
               best-result (if  (> try-result best-result) try-result best-result)
               best-x (if (= best-result try-result) try-x best-x)
               best-y (if (= best-result try-result) try-y best-y)]
@@ -25,10 +34,27 @@
   [f x y h]
   (/ (- (f (+ x h) y) (f x y)) h))
 
-(defn derivative-tweak
+(defn numerical-derivative-tweak
   [f x y]
   (let [h 0.0001
         step-size 0.01
         x (+ x (* step-size (-derivative f x y h)))
         y (+ y (* step-size (-derivative f y x h)))]
     {:result (f x y) :x x :y x}))
+
+(defn analytical-forward-circuit
+  [x y z]
+  (let [q (forward-add-gate x y)
+        f (forward-multiply-gate q z)
+        der-f-wrt-z q
+        der-f-wrt-q z
+        der-q-wrt-x 1.0
+        der-q-wrt-y 1.0
+        der-f-wrt-x (* der-q-wrt-x der-f-wrt-q)
+        der-f-wrt-y (* der-q-wrt-y der-f-wrt-q)
+        der-f-wrt-xyz [der-f-wrt-x der-f-wrt-y der-f-wrt-z]
+        step-size 0.01
+        x (+ x (* step-size der-f-wrt-x))
+        y (+ y (* step-size der-f-wrt-y))
+        z (+ z (* step-size der-f-wrt-z))]
+    (forward-multiply-gate (forward-add-gate x y) z)))
--- test/rnn/core_test.clj
+++ test/rnn/core_test.clj
@@ -3,11 +3,17 @@
             [rnn.core :refer :all]))
 
 (deftest gate-tests
-  (testing "Testing forward multiplier gate."
-    (is (= -6 (forward-multiplier-gate -2 3))))
+  (testing "Testing forward multiply gate."
+    (is (= -6 (forward-multiply-gate -2 3))))
   (testing "Try to get something better than -6 by randomly tweaking the inputs."
-    (is (> (:result (random-tweak forward-multiplier-gate -2 3))
+    (is (> (:result (random-tweak forward-multiply-gate -2 3))
            -6)))
   (testing "Try to get something better than -6 using derivatives"
-    (is (> (:result (derivative-tweak forward-multiplier-gate -2 3))
-           -6))))
+    (is (> (:result (numerical-derivative-tweak forward-multiply-gate -2 3))
+           -6)))
+  (testing "Testing forward circuit"
+    (is (= (forward-circuit -2 5 -4)
+           -12)))
+  (testing "optimize "
+    (is (> (analytical-forward-circuit -2 5 -4)
+           -12))))
