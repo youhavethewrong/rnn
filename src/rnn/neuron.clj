b86e6635f6285e706a4c6b1c5525dcdbbdeea231
ESC <youhavethewrong@gmail.com>
2015-07-11 19:54:01 -0500

    Implement add gate.

--- src/rnn/neuron.clj
+++ src/rnn/neuron.clj
@@ -7,6 +7,17 @@
   (forward [gate u0 u1] "Output value resulting from operating on the input values.")
   (backward [gate o0 u0 u1] "Chain the output gradient to the input gradients."))
 
+(defrecord AddGate []
+  Gate
+  (forward
+   [_ u0 u1]
+   {:value (+ (:value u0) (:value u1)) :gradient 0.0})
+  
+  (backward
+   [_ o0 u0 u1]
+   [(assoc u0 :gradient (+ (:gradient u0) (+ 1.0 (:gradient o0))))
+    (assoc u1 :gradient (+ (:gradient u1) (+ 1.0 (:gradient o0))))]))
+
 (defrecord MultiplyGate []
   Gate
   (forward
@@ -16,5 +27,4 @@
   (backward
    [_ o0 u0 u1]
    [(assoc u0 :gradient (+ (:gradient u0) (* (:value u1) (:gradient o0))))
-    (assoc u1 :gradient (+ (:gradient u1) (* (:value u0) (:gradient o0))))])
-)
+    (assoc u1 :gradient (+ (:gradient u1) (* (:value u0) (:gradient o0))))]))
--- test/rnn/neuron_test.clj
+++ test/rnn/neuron_test.clj
@@ -1,7 +1,21 @@
 (ns rnn.neuron-test
   (:require [rnn.neuron :refer :all]
             [clojure.test :refer :all])
-  (:import [rnn.neuron MultiplyGate Unit]))
+  (:import [rnn.neuron AddGate MultiplyGate Unit]))
+
+(deftest add-gate
+  (testing "Should exercise forward and backword propagation for the add gate."
+    (let [gate (AddGate.)
+          forward-value (.forward gate
+                                  (Unit. 2 0.0)
+                                  (Unit. 3 0.0))
+          backward-value (.backward gate
+                                    forward-value
+                                    (Unit. 2 0.0)
+                                    (Unit. 3 0.0))]
+      (is (= {:value 5 :gradient 0.0} forward-value))
+      (is (= [(map->Unit {:value 2 :gradient 1.0})
+              (map->Unit {:value 3 :gradient 1.0})] backward-value)))))
 
 (deftest multiply-gate
   (testing "Should exercise forward and backword propagation for the multiply gate."
