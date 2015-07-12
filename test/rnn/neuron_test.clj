cbab5e64a4b494fe97816f5fa7654514b4bd00f5
ESC <youhavethewrong@gmail.com>
2015-07-12 16:38:55 -0500

    Add a neuron with a simple forward gate setup.

--- test/rnn/neuron_test.clj
+++ test/rnn/neuron_test.clj
@@ -45,3 +45,24 @@
       (is (double= 0.88079 (:value forward-value) 0.001))
       (is (= 0.0 (:gradient forward-value)))
       (is (= (map->Unit {:value 2 :gradient 0.0}) backward-value)))))
+
+(deftest two-dimensional-neuron
+  (testing "Set up a simple 2d neuron using 2 multiply gates, 2 add gates, and a sig gate."
+    (let [a (Unit. 1.0 0.0)
+          b (Unit. 2.0 0.0)
+          c (Unit. -3.0 0.0)
+          x (Unit. -1.0 0.0)
+          y (Unit. 3.0 0.0)
+          mulg0 (MultiplyGate.)
+          mulg1 (MultiplyGate.)
+          addg0 (AddGate.)
+          addg1 (AddGate.)
+          sg0 (SigmoidGate.)
+          forward-neuron (.forward sg0
+                                   (.forward addg1
+                                             (.forward addg0
+                                                       (.forward mulg0 a x)
+                                                       (.forward mulg1 b y))
+                                             c) nil)]
+      (is (double= 0.8808 (:value forward-neuron) 0.0001))
+      )))
